dd82f25be32e6c571e5f055031a073901e2e4518
kakashi7860 <sohelshakh91@gmail.com>
2026-01-29 17:48:33 +0530

    hello

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home';
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>hello</h1>
+     <Home/>
+        
+    </>
+  )
+}
+
+export default App
